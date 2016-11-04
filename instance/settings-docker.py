462cb78a3b2f70bd85a3a389960723995cd9127c
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2016-11-04 18:06:09 +0200

    eval FRAME_COOKES change to ast.literal_eval

--- instance/settings-docker.py
+++ instance/settings-docker.py
@@ -1,13 +1,14 @@
+import ast
 import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DATABASE_URI = os.environ.get('DATABASE_URI')
 FRAME_URL = os.environ.get('FRAME_URL')
-HTTP_PROXIED = eval(os.environ.get('HTTP_PROXIED'))
+HTTP_PROXIED = ast.literal_eval(os.environ.get('HTTP_PROXIED'))
 HTTP_X_SCRIPT_NAME = os.environ.get('HTTP_X_SCRIPT_NAME')
-ZOPE_TEMPLATE_CACHE = eval(os.environ.get('ZOPE_TEMPLATE_CACHE'))
-FRAME_COOKIES = os.environ.get('FRAME_COOKIES')
-MAIL_FAIL_SILENTLY = eval(os.environ.get('MAIL_FAIL_SILENTLY'))
+ZOPE_TEMPLATE_CACHE = ast.literal_eval(os.environ.get('ZOPE_TEMPLATE_CACHE'))
+FRAME_COOKIES = ast.literal_eval(os.environ.get('FRAME_COOKIES'))
+MAIL_FAIL_SILENTLY = ast.literal_eval(os.environ.get('MAIL_FAIL_SILENTLY'))
 DEFAULT_MAIL_SENDER = os.environ.get('DEFAULT_MAIL_SENDER')
-ADMINISTRATOR_EMAILS = eval(os.environ.get('ADMINISTRATOR_EMAILS'))
+ADMINISTRATOR_EMAILS = ast.literal_eval(os.environ.get('ADMINISTRATOR_EMAILS'))
